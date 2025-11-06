2a38aa6fcaeb9f21376c0e5ffe22b1c6be78bc63
Marius Murtz <mariusmurtz@MBPM23.local>
2025-11-06 19:12:23 +0100

    Update manifest, client.js und index.html

--- public/client.js
+++ public/client.js
@@ -1,11 +1,11 @@
 window.TrelloPowerUp.initialize({
-  'card-back-section': function(t, options){
+  'card-back-section': function(t, options) {
     return {
       title: 'Work Size',
       icon: 'https://deinbaum.github.io/trello-worksize/public/appicon.png',
       content: {
         type: 'iframe',
-        url: t.absoluteUrl('public/index.html'),
+        url: t.absoluteUrl('./index.html'), // relativ zum public-Ordner
         height: 230
       }
     };
